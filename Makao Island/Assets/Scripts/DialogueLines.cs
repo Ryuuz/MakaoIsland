445d1ef861be607f1be6e57825ba6d1f8e36aeb0
Ryuuz <cmodegaard@gmail.com>
2019-01-22 07:39:29 +0100

    User Interface
    -Started on the dialogue system
    -Added canvas for UI

---
+++ Makao Island/Assets/Scripts/DialogueLines.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Sentence
+{
+    public int speaker;
+    public string text;
+
+    public Sentence(int n, string s)
+    {
+        speaker = n;
+        text = s;
+    }
+}
+
+public class DialogueLines : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
