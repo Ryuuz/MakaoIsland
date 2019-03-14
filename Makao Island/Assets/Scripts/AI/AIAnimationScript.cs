80d2471d715ef4c22d72dbe0f3a5ba37db08d1f4
Ryuuz <cmodegaard@gmail.com>
2019-03-14 22:18:02 +0100

    Animations for NPCs
    -NPCs for adult villagers now use animations
    -Fixed a bug with the dialogue box
    -Created scripts for controlling animations
    -Split each type of NPC into male and female versions

---
+++ Makao Island/Assets/Scripts/AI/AIAnimationScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIAnimationScript : MonoBehaviour
+{
+    protected Animator mAnimator;
+    protected GameManager mGameManager;
+
+    protected virtual void Start()
+    {
+        mAnimator = GetComponent<Animator>();
+        mGameManager = GameManager.ManagerInstance();
+        mGameManager.eSpeedChanged.AddListener(SetPlaySpeed);
+        SetPlaySpeed(mGameManager.mGameSpeed);
+    }
+
+    public void SetPlaySpeed(float speed)
+    {
+        mAnimator.SetFloat("GameSpeed", speed);
+    }
+}
---
+++ Makao Island/Assets/Scripts/AI/VillagerAnimationScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class VillagerAnimationScript : AIAnimationScript
+{
+    protected NavMeshAgent mAgent;
+    protected float mCurrentTime;
+    protected bool mSwitchIdle = false;
+
+    protected override void Start()
+    {
+        base.Start();
+        mAgent = GetComponentInParent<NavMeshAgent>();
+        mCurrentTime = Random.Range(5f, 12f);
+        mAnimator.Play("idle_normal", 0, Random.value);
+    }
+
+    void Update()
+    {
+        mCurrentTime -= Time.deltaTime;
+        if(mCurrentTime <= 0f)
+        {
+            mSwitchIdle = true;
+            mCurrentTime = Random.Range(5f, 12f);
+        }
+
+        if(mAgent.velocity != Vector3.zero)
+        {
+            if(!mAnimator.GetBool("Walking"))
+            {
+                mAnimator.SetBool("Walking", true);
+            }
+            
+            mAnimator.SetFloat("WalkSpeed", Mathf.Max(mAgent.velocity.magnitude / mAgent.speed, 0.3f));
+        }
+        else
+        {
+            if(mAnimator.GetBool("Walking"))
+            {
+                mAnimator.SetBool("Walking", false);
+            }
+
+            if(mAnimator.GetCurrentAnimatorStateInfo(0).IsName("idle_normal") && mSwitchIdle)
+            {
+                mSwitchIdle = false;
+                if(Random.value < 0.4f)
+                {
+                    if(Random.value < 0.5f)
+                    {
+                        mAnimator.SetTrigger("Lean");
+                    }
+                    else
+                    {
+                        mAnimator.SetTrigger("Turn");
+                    }
+                }
+            }
+            else if(!mAnimator.GetCurrentAnimatorStateInfo(0).IsName("idle_normal"))
+            {
+                mAnimator.ResetTrigger("Lean");
+                mAnimator.ResetTrigger("Turn");
+            }
+        }
+    }
+}
