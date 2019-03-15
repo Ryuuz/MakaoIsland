ce1246010a845a30cb3a130ac17ad6a85f675755
Ryuuz <cmodegaard@gmail.com>
2019-03-15 17:26:03 +0100

    More NPCs with animations
    -Implemented animations on all imported NPCs
    -Added girl and boy under each NPC type
    -Made animation script for child
    -Updated the NPC instances in the scene to use the new models

---
+++ Makao Island/Assets/Scripts/AI/ChildAnimationScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChildAnimationScript : AIAnimationScript
+{
+    protected NavMeshAgent mAgent;
+
+    protected override void Start()
+    {
+        base.Start();
+        mAgent = GetComponentInParent<NavMeshAgent>();
+        mAnimator.Play("Child_Idle_Animation", 0, Random.value);
+    }
+
+    void Update()
+    {
+        if (mAgent.velocity != Vector3.zero)
+        {
+            if (!mAnimator.GetBool("Walking"))
+            {
+                mAnimator.SetBool("Walking", true);
+            }
+
+            mAnimator.SetFloat("WalkSpeed", Mathf.Max(mAgent.velocity.magnitude / mAgent.speed, 0.3f));
+        }
+        else
+        {
+            if (mAnimator.GetBool("Walking"))
+            {
+                mAnimator.SetBool("Walking", false);
+            }
+        }
+    }
+}
