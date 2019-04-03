538fe2e37fcd9aa4e2cf2434fb9f5cf2684d97c8
Ryuuz <cmodegaard@gmail.com>
2019-04-03 21:33:29 +0200

    Pandamoose changes
    -First version of glowing orb / mist the player can follow

---
+++ Makao Island/Assets/Scripts/SpiritOrbScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SpiritOrbScript : MonoBehaviour
+{
+    [SerializeField]
+    private Transform mGoal;
+
+    private NavMeshAgent mAgent;
+
+    void Start()
+    {
+        mAgent = GetComponent<NavMeshAgent>();
+        mGoal = GameObject.Find("OrbGoal").transform;
+        if(mGoal)
+        {
+            mAgent.SetDestination(mGoal.position);
+            mAgent.isStopped = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            mAgent.isStopped = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            mAgent.isStopped = true;
+        }
+    }
+}
