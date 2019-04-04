1ead6b114a70266155d20a012fa6521f72156a00
Ryuuz <cmodegaard@gmail.com>
2019-04-04 22:16:04 +0200

    More work on pandamoose
    -The spirit orb will now try to keep up with the player
    -The spirit orb will vanish soon after reaching its destination
    -Upon vanishing, the spirit orb passes on its knowledge to the player (map becomes available)
    -Updated the bounds used for the map UI

--- Makao Island/Assets/Scripts/SpiritOrbScript.cs
+++ Makao Island/Assets/Scripts/SpiritOrbScript.cs
@@ -6,13 +6,17 @@ using UnityEngine.AI;
 public class SpiritOrbScript : MonoBehaviour
 {
     [SerializeField]
-    private Transform mGoal;
+    private GameObject mMapTutorial;
 
+    private Transform mGoal;
     private NavMeshAgent mAgent;
+    private bool mGoalReached = false;
+    private CharacterController mPlayerController;
 
     void Start()
     {
         mAgent = GetComponent<NavMeshAgent>();
+        mPlayerController = GameManager.ManagerInstance().mPlayer.GetComponent<CharacterController>();
         mGoal = GameObject.Find("OrbGoal").transform;
         if(mGoal)
         {
@@ -29,11 +33,44 @@ public class SpiritOrbScript : MonoBehaviour
         }
     }
 
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            float playerSpeed = mPlayerController.velocity.magnitude;
+            mAgent.speed = (playerSpeed > 4f) ? (playerSpeed + 1f) : mAgent.speed;
+
+            //Debug.Log(mAgent.hasPath);
+            if(!mAgent.hasPath && !mAgent.pathPending && !mGoalReached)
+            {
+                mGoalReached = true;
+                StartCoroutine(Vanish());
+            }
+        }
+    }
+
     private void OnTriggerExit(Collider other)
     {
         if(other.tag == "Player")
         {
             mAgent.isStopped = true;
         }
+    }
+
+    private IEnumerator Vanish()
+    {
+        yield return new WaitForSeconds(3f);
+        GetComponentInChildren<ParticleSystem>().Stop();
+        yield return new WaitForSeconds(5f);
+
+        InputHandler.InputInstance().mMapManager.mMapAvailable = true;
+
+        if (mMapTutorial)
+        {
+            GameObject temp = Instantiate(mMapTutorial, transform.position, Quaternion.identity);
+            temp.GetComponent<ControlTutorial>().mAction = ControlAction.map;
+        }
+
+        Destroy(gameObject);
     }
 }
