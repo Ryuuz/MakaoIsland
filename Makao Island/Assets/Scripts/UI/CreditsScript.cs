a02d2ac211e491f6285264811aad9d5a62907069
Ryuuz <cmodegaard@gmail.com>
2019-04-07 20:59:01 +0200

    The game's goal
    -The game will now end once the player enters the stonegate
    -Added a credits scene
    -At the end, the screen will white out, then switch to the credits

---
+++ Makao Island/Assets/Scripts/UI/CreditsScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsScript : MonoBehaviour
+{
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void GoToMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+}
--- Makao Island/Assets/Scripts/WhiteOutScript.cs
+++ Makao Island/Assets/Scripts/WhiteOutScript.cs
@@ -1,18 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class WhiteOutScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private CanvasGroup mCanvasGroup;
+
+    private Image mBackground;
+    private Renderer mRenderer;
+
     void Start()
     {
-        
+        mBackground = mCanvasGroup.GetComponentInChildren<Image>();
+        mRenderer = GetComponentInChildren<Renderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if(other.tag == "Player")
+        {
+            StartCoroutine(WhiteOut());
+        }
+    }
+
+    private IEnumerator WhiteOut()
+    {
+        mCanvasGroup.gameObject.SetActive(true);
+        mCanvasGroup.alpha = 0f;
+        mBackground.color = Color.white;
+
+        Color startColor = mRenderer.material.GetColor("_EmissionColor");
+        Color endColor = Color.white * 20f;
+        Color emissiveColor;
+
+        float lerpTime = 0f;
+
+        InputHandler.InputInstance().StartCutscene(10f);
+
+        while(lerpTime <= 1f)
+        {
+            lerpTime += Time.deltaTime * 0.5f;
+            mCanvasGroup.alpha = Mathf.Lerp(0f, 1f, lerpTime);
+            emissiveColor = Color.Lerp(startColor, endColor, lerpTime);
+
+            mRenderer.material.SetColor("_EmissionColor", emissiveColor);
+            DynamicGI.SetEmissive(mRenderer, emissiveColor);
+
+            yield return null;
+        }
+
+        SceneManager.LoadScene(2);
     }
 }
