76f59099d7c907161685935d8fa938fa976ab9ed
Ryuuz <cmodegaard@gmail.com>
2019-04-12 18:53:01 +0200

    Script for making objects float
    -Made a very simple script for having objects follow the waves on the water

--- Makao Island/Assets/Scripts/FollowWaterScript.cs
+++ Makao Island/Assets/Scripts/FollowWaterScript.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class FollowWaterScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject mOcean;
+
+    private Transform mTransform;
+    private float mAmount;
+    private float mSpeed;
+    private float mHeight;
+    private float mOceanPositionY;
+    private Vector3 mPosition;
+
     void Start()
     {
-        
+        Material oceanMaterial = mOcean.GetComponent<Renderer>().material;
+        mTransform = GetComponent<Transform>();
+
+        mOceanPositionY = mOcean.GetComponent<Transform>().position.y;
+        mSpeed = oceanMaterial.GetFloat("_Speed");
+        mHeight = oceanMaterial.GetFloat("_Height");
+        mAmount = oceanMaterial.GetFloat("_Amount");
+
+        mTransform.position.Set(mTransform.position.x, mOceanPositionY, mTransform.position.z);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        mPosition = mTransform.position;
+
+        mPosition.y = mOceanPositionY + Mathf.Sin((Time.time * 2f) * mSpeed + (mPosition.x * mPosition.z * mAmount)) * mHeight;
+        mTransform.position = mPosition;
     }
 }
